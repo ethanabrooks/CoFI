1c1c3bf255d743cfcf2052a712b5e83ba0f450f6
Ethan Brooks <ethanabrooks@gmail.com>
2021-01-17 21:34:10 -0500

    better error messages for depencency not met

--- data_types.py
+++ data_types.py
@@ -779,9 +779,9 @@ class BuildingAction(HasWorkers, CoordCanOpenGate):
     ) -> Optional[str]:
         dependency = dependencies[self.building]
         dependency_met = dependency in [*building_positions.values(), None]
-        insufficient_resources = Counter(self.building.cost) - resources
         if not dependency_met:
-            return f"Dependency ({dependency}) not met"
+            return f"Dependency ({dependency}) not met for {self}."
+        insufficient_resources = Counter(self.building.cost) - resources
         if insufficient_resources:
             return "Insufficient resources"
         return None
@@ -813,8 +813,9 @@ class BuildingCoordAction(HasWorkers, NoWorkersAction):
         pending_positions: BuildingPositions,
         positions: Positions,
     ) -> Optional[str]:
-        if not dependencies[self.building] in [*building_positions.values(), None]:
-            return "Dependency not met"
+        dependency = dependencies[self.building]
+        if not dependency in [*building_positions.values(), None]:
+            return f"Dependency ({dependency}) not met for {self.building}."
         coord = astuple(self.coord)
         all_positions = {**building_positions, **pending_positions}
         if coord in all_positions:
